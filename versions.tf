1b18c75da25ed9a5255090fc615feac663d05a45
Aiswaryasharma <134687084+Aiswaryasharma@users.noreply.github.com>
2023-06-09 16:36:49 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "SmithaOrg"
+    organization = "AiswaryaTerraform"
 
     workspaces {
       name = "sentinel-example"
